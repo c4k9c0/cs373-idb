95cdda348851893fbce6f09a5dbe81f411650728
Gabriel Gontijo Moreira <gabrielgo.moreira@egmail.com>
2016-03-20 18:00:10 -0500

    Created the crimes model to mirror the crimes.json and tabularized the other columns initializations

--- app/models.py
+++ app/models.py
@@ -7,27 +7,27 @@ Models for Players
 class Players(db.Model):
 	__tablename__ = 'players'
 
-	player_id = db.Column(db.Integer, primary_key=True)
+	player_id     = db.Column(db.Integer, primary_key = True)
 
-	last_arrest = db.Column(db.Date)
-	name = db.Column(db.String(80))
-	pos = db.Column(db.String(5))
-	first_name = db.Column(db.String(80))
-	team = db.Column(db.String(5))
-	last_name = db.Column(db.String(80))
-	num_arrests = db.Column(db.Integer)
+	last_arrest   = db.Column(db.Date)
+	name          = db.Column(db.String(80))
+	pos           = db.Column(db.String(5))
+	first_name    = db.Column(db.String(80))
+	team          = db.Column(db.String(5))
+	last_name     = db.Column(db.String(80))
+	num_arrests   = db.Column(db.Integer)
 
 
 
 	def __init__(self, player_id, last_arrest, name, pos, first_name, team, last_name, num_arrests):
-		self.player_id = player_id
+		self.player_id   = player_id
 		self.last_arrest = last_arrest
-		self.name = name
-		self.pos = pos
-		self.first_name = first_name
-		self.team = team
-		self.last_name = last_name
-		self.num_arrest = num_arrest
+		self.name        = name
+		self.pos         = pos
+		self.first_name  = first_name
+		self.team        = team
+		self.last_name   = last_name
+		self.num_arrest  = num_arrest
 
 """
 Models for Teams
@@ -44,5 +44,23 @@ Models for Crimes
 class Crimes(db.Model):
 	__tablename__ = 'crimes'
 
-	crimes_id = db.Column(db.Integer, primary_key=True)
+	crime_id      = db.Column(db.Integer, primary_key = True)
+
+	team          = db.Column(db.String(5))
+	date          = db.Column(db.Date)
+	description   = db.Column(db.Text)
+    position      = db.Column(db.String(5))
+    outcome       = db.Column(db.Text)
+    category      = db.Column(db.String(80))
+    encounter     = db.Column(db.String(10))
+
+    def __init__(self, crime_id, team, date, description, position, outcome, category, encounter):
+        self.crime_id    = crime_id
+        self.team        = team
+        self.date        = date
+        self.description = description
+        self.position    = position
+        self.outcome     = outcome
+        self.category    = category
+        self.encounter   = encounter
 
