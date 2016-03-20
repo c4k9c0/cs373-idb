21eecc3a9b95f7e812610471baea29e28a47fe07
Gabriel Gontijo Moreira <gabrielgo.moreira@egmail.com>
2016-03-20 17:26:01 -0500

    Updated the columns of the players model

--- app/models.py
+++ app/models.py
@@ -1,5 +1,36 @@
 from sqlalchemy import *
-from flask_app import db
+from app import db
+
+"""
+Models for Players
+"""
+class Players(db.Model):
+	__tablename__ = 'players'
+
+	player_id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(80))
+	team = db.Column(db.String(5))
+	position = db.Column(db.String(5))
+	arrests = db.Column(db.Integer)
+	last_arrest = db.Column(db.Date)
+
+	def __init__(self, player_id, name, team, position, arrests, last_arrest):
+		self.player_id = player_id
+		self.name = name
+		self.team = team
+		self.position = position
+		self.arrests = arrests
+		self.last_arrest = last_arrest
+
+
+
+"""
+Models for Teams
+"""
+class Teams(db.Model):
+	__tablename__ = 'teams'
+
+	teams_id = db.Column(db.Integer, primary_key=True)
 
 
 """
@@ -7,18 +38,6 @@ Models for Crimes
 """
 class Crimes(db.Model):
 	__tablename__ = 'crimes'
-	crimes_id = Column(Integer, primary_key=True)
 
-"""
-Models for Players
-"""
-class Players(db.Model):
-	__tablename__ = 'players'
-	players_id = Column(Integer, primary_key=True)
+	crimes_id = db.Column(db.Integer, primary_key=True)
 
-"""
-Models for Teams
-"""
-class Teams(db.Model):
-	__tablename__ = 'teams'
-	teams_id = Column(Integer, primary_key=True)
