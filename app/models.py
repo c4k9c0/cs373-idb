718c5d8c07bdb80907e5560f72a21358d4b8fe64
Gabriel Gontijo Moreira <gabrielgo.moreira@egmail.com>
2016-03-20 17:37:49 -0500

    mirror the players.json to improve the columns from the players model

--- app/models.py
+++ app/models.py
@@ -8,21 +8,26 @@ class Players(db.Model):
 	__tablename__ = 'players'
 
 	player_id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(80))
-	team = db.Column(db.String(5))
-	position = db.Column(db.String(5))
-	arrests = db.Column(db.Integer)
+
 	last_arrest = db.Column(db.Date)
+	name = db.Column(db.String(80))
+	pos = db.Column(db.String(5))
+	first_name = db.Column(db.String(80))
+	team = db.Column(db.String(5))
+	last_name = db.Column(db.String(80))
+	num_arrests = db.Column(db.Integer)
 
-	def __init__(self, player_id, name, team, position, arrests, last_arrest):
+
+
+	def __init__(self, player_id, last_arrest, name, pos, first_name, team, last_name, num_arrests):
 		self.player_id = player_id
-		self.name = name
-		self.team = team
-		self.position = position
-		self.arrests = arrests
 		self.last_arrest = last_arrest
-
-
+		self.name = name
+		self.pos = pos
+		self.first_name = first_name
+		self.team = team
+		self.last_name = last_name
+		self.num_arrest = num_arrest
 
 """
 Models for Teams
