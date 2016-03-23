7f2fd2ae1aac0f11a958692862ec6c349e0a677a
c4k9c0 <charliecox17@yahoo.com>
2016-03-22 19:00:11 -0500

    Adding models and pydoc #24

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from sqlalchemy import *
-from app import db
+
+# This is only because models.py has to be in the root.
+from app.app import db
 
 """
 Models for Players
@@ -8,12 +10,11 @@ class Players(db.Model):
 	__tablename__ = 'players'
 
 	player_id     = db.Column(db.Integer, primary_key = True)
-
 	last_arrest   = db.Column(db.Date)
 	name          = db.Column(db.String(80))
 	pos           = db.Column(db.String(5))
 	first_name    = db.Column(db.String(80))
-	team          = db.Column(db.String(5))
+	team          = db.Column(db.ForeignKey("teams.team_id")) # This is a foriegn key
 	last_name     = db.Column(db.String(80))
 	num_arrests   = db.Column(db.Integer)
 
@@ -36,19 +37,19 @@ class Teams(db.Model):
 	__tablename__ = 'teams'
 
 	team_id       = db.Column(db.Integer, primary_key = True)
-    city          = db.Column(db.String(80))
-    state         = db.Column(db.String(80))
-    mascot        = db.Column(db.String(80))
-    division      = db.Column(db.String(80))
-    championships = db.Column(db.Integer)
+	city          = db.Column(db.String(80))
+	state         = db.Column(db.String(80))
+	mascot        = db.Column(db.String(80))
+	division      = db.Column(db.String(80))
+	championships = db.Column(db.Integer)
 
-    def __init__(self, team_id, city, state, mascot, division, championships):
-        self.team_id       = team_id
-        self.city          = city
-        self.state         = state
-        self.mascot        = mascot
-        self.division      = division
-        self.championships = championships
+	def __init__(self, team_id, city, state, mascot, division, championships):
+		self.team_id       = team_id
+		self.city          = city
+		self.state         = state
+		self.mascot        = mascot
+		self.division      = division
+		self.championships = championships
 
 """
 Models for Crimes
@@ -58,21 +59,22 @@ class Crimes(db.Model):
 
 	crime_id      = db.Column(db.Integer, primary_key = True)
 
-	team          = db.Column(db.String(5))
+	team          = db.Column(db.ForeignKey("teams.team_id")) # F Key
 	date          = db.Column(db.Date)
 	description   = db.Column(db.Text)
-    position      = db.Column(db.String(5))
-    outcome       = db.Column(db.Text)
-    category      = db.Column(db.String(80))
-    encounter     = db.Column(db.String(10))
+	position      = db.Column(db.String(5))
+	outcome       = db.Column(db.Text)
+	category      = db.Column(db.String(80))
+	encounter     = db.Column(db.String(10))
+	name  		  = db.Column(db.ForeignKey("players.player_id"))
 
-    def __init__(self, crime_id, team, date, description, position, outcome, category, encounter):
-        self.crime_id    = crime_id
-        self.team        = team
-        self.date        = date
-        self.description = description
-        self.position    = position
-        self.outcome     = outcome
-        self.category    = category
-        self.encounter   = encounter
-
+	def __init__(self, crime_id, team, date, description, position, outcome, category, encounter, name):
+		self.crime_id    = crime_id
+		self.team        = team
+		self.date        = date
+		self.description = description
+		self.position    = position
+		self.outcome     = outcome
+		self.category    = category
+		self.encounter   = encounter
+		self.name  		 = name
