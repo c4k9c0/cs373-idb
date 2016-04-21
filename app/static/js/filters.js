ff9269ee8203e02b8682bd13c374d1384bd9e08a
c4k9c0 <charliecox17@yahoo.com>
2016-04-20 21:49:30 -0500

    Adding filters.js

---
+++ app/static/js/filters.js
@@ -0,0 +1,21 @@
+var nflCsFilters = angular.module('nflCsFilters', ['ngSanitize']);
+
+nflCsFilters.filter('searchFilter', function() {
+        return function (input, query) {
+        	// input =Ricky Williams query = "Ricky RB"
+        	var splitWords = query.split(" ");
+
+        	console.log(splitWords);
+
+        	if(splitWords.length < 1) {
+        		return input.replace(RegExp('('+ query + ')', 'g'), '<span class="bold-match">$1</span>');           
+        	} else {
+        		for (var w in splitWords) {
+        			//console.log(word);
+        			input = input.replace(RegExp('('+ splitWords[w] + ')', 'g'), '<span class="bold-match">$1</span>');
+        		}
+        		return input;
+        	}
+            
+        }
+    });
